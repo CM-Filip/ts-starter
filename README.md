69369491f80835e927b4d1ecfb2668459af419b8
Filip Iliev <filip.iliev@kontron.mk>
2024-04-24 18:50:20 +0200

    Initial setup; webpack config; ts config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ts-starter
+# TypeScript Starter
 A webpack-typescript starter kit for new developers
+
+# Prerequisites
+This project uses:
+  TypeScript | npm install typescript --save-dev
+  TS Loader | npm install ts-loader
+  Webpack | npm install webpack --save-dev
+  Webpack CLI | npm install webpack-cli --save-dev
+  Webpack Development Server npm install webpack-dev-server --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist') // Directory to serve the files from
+    },
+    compress: true,
+    port: 4200,
+    open: true
+  }
+}
